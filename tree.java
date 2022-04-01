a077151a07a4ee6a51345582a63e7fd6199597f6
edenrstapleton <102817586+edenrstapleton@users.noreply.github.com>
2022-04-01 16:55:57 -0400

    Create tree.java

---
+++ tree.java
@@ -0,0 +1,14 @@
+public class Tree{
+  public static void main (String[] args) {
+    System.out.println("Hello! My name is Eden and I'm learning to code in Java!");
+    System.out.println("Today, we're creating trees in code!");
+    System.out.println("    *   ");
+    System.out.println("  (   ) ");
+    System.out.println("  (   ) ");
+    System.out.println("    |   ");
+    System.out.println("~~~~~~~~");
+
+
+
+  }
+}
